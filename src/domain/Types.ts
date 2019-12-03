90207c1f3c437bd91275df1e25a3705f44a31f99
AnuchitO <anuchitp@thoughtworks.com>
2019-12-03 22:23:18 +0700

    delete vue project

--- src/domain/Types.ts
+++
@@ -1,8 +0,0 @@
-export type Transaction = {
-  payer: string;
-  owes: string[];
-  amount: number;
-};
-
-export type AccountTable = number[][];
-export type AccountIndex = { [key: string]: number; };
