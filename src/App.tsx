a69e90792e75cbb50977a1b08167e77160dd8116
AnuchitO <anuchitp@thoughtworks.com>
2019-12-04 23:34:57 +0700

    draft elements

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,49 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* expenses */}
+        {/* Amount */}
+        <input type="number" onChange={(e) => console.log(e.target.value)} placeholder="0 bath" />
+
+        {/* Members */}
+        <div>
+          <div>
+            <input type="checkbox" id="1" name="members" onChange={(e) => console.log(e.target.value)} checked />
+            <label htmlFor="1">AnuchitO</label>
+          </div>
+          <div>
+            <input type="checkbox" id="2" name="members" onChange={(e) => console.log(e.target.value)} checked />
+            <label htmlFor="2">Kob</label>
+          </div>
+          <div>
+            <input type="checkbox" id="3" name="members" onChange={(e) => console.log(e.target.value)} checked />
+            <label htmlFor="3">Tom</label>
+          </div>
+          <div>
+            <input type="checkbox" id="4" name="members" onChange={(e) => console.log(e.target.value)} checked />
+            <label htmlFor="4">offlane</label>
+          </div>
+        </div>
+
+        {/* Note */}
+        <label htmlFor="note">Note</label>
+        <select name="note" id="note" >
+          <option value="food">Food</option>
+          <option value="drink">Drink</option>
+          <option value="fuel">Fuel</option>
+          <option value="snack">Snack</option>
+        </select>
+
+
+
       </header>
+
+
+      <button type="button">
+        Add to favorites
+        </button>
+
+
     </div>
   );
 }
