a29b7607f5756478d7ea8d18f38d1def6b214633
AnuchitO <anuchitp@thoughtworks.com>
2019-12-05 18:05:02 +0700

    binding amount to state

--- src/Expenses.tsx
+++ src/Expenses.tsx
@@ -1,44 +1,64 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
 
-const Expenses: React.FC = () => {
-  return (
-    <div>
-      {/* Amount */}
-      <input type="number" onChange={(e) => console.log(e.target.value)} placeholder="0 bath" />
 
-      {/* Members */}
-      <div>
-        <div>
-          <input type="checkbox" id="1" name="members" onChange={(e) => console.log(e.target.value)} checked />
-          <label htmlFor="1">AnuchitO</label>
-        </div>
-        <div>
-          <input type="checkbox" id="2" name="members" onChange={(e) => console.log(e.target.value)} checked />
-          <label htmlFor="2">Kob</label>
-        </div>
-        <div>
-          <input type="checkbox" id="3" name="members" onChange={(e) => console.log(e.target.value)} checked />
-          <label htmlFor="3">Tom</label>
-        </div>
-        <div>
-          <input type="checkbox" id="4" name="members" onChange={(e) => console.log(e.target.value)} checked />
-          <label htmlFor="4">offlane</label>
-        </div>
-      </div>
+export type Transaction = {
+  payer: string;
+  owes: string[];
+  amount: number;
+};
 
-      {/* Note */}
-      <label htmlFor="note">Note</label>
-      <select name="note" id="note" >
-        <option value="food">Food</option>
-        <option value="drink">Drink</option>
-        <option value="fuel">Fuel</option>
-        <option value="snack">Snack</option>
-      </select>
 
-      <button type="button" onClick={() => console.log("save")}>Save</button>
+class Expenses extends React.Component {
+  constructor(props: any) {
+    super(props)
 
-    </div>
-  )
+    this.state = {
+      payer: "",
+      owes: [],
+      amount: 0
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        {/* Amount */}
+        < input type="number" onChange={(e) => this.setState({ amount: e.target.value })} placeholder="0 bath" />
+
+        {/* Members */}
+        < div >
+          <div>
+            <input type="checkbox" id="1" name="members" onChange={(e) => console.log(e.target.value)} checked />
+            <label htmlFor="1">AnuchitO</label>
+          </div>
+          <div>
+            <input type="checkbox" id="2" name="members" onChange={(e) => console.log(e.target.value)} checked />
+            <label htmlFor="2">Kob</label>
+          </div>
+          <div>
+            <input type="checkbox" id="3" name="members" onChange={(e) => console.log(e.target.value)} checked />
+            <label htmlFor="3">Tom</label>
+          </div>
+          <div>
+            <input type="checkbox" id="4" name="members" onChange={(e) => console.log(e.target.value)} checked />
+            <label htmlFor="4">offlane</label>
+          </div>
+        </div >
+
+        {/* Note */}
+        < label htmlFor="note" > Note</label >
+        <select name="note" id="note" >
+          <option value="food">Food</option>
+          <option value="drink">Drink</option>
+          <option value="fuel">Fuel</option>
+          <option value="snack">Snack</option>
+        </select>
+
+        <button type="button" onClick={() => console.log(this.state)}>Save</button>
+
+      </Fragment >
+    )
+  }
 }
 
 export default Expenses;
