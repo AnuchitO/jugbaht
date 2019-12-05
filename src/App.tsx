a00bc500cfbb7d2582cdacfc27c938b2b59363f4
AnuchitO <anuchitp@thoughtworks.com>
2019-12-05 21:48:26 +0700

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 import './App.css'
 import Expenses from './Expenses'
+import Summary from './Summary'
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Expenses />
+      <Router>
+        <Route path="/" exact component={Expenses} />
+        <Route path="/summary" component={Summary} />
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
