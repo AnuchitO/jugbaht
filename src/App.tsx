c6a8e08a1f449aae2f3f12ff15a75827903c6ed5
AnuchitO <anuchitp@thoughtworks.com>
2019-12-07 00:52:11 +0700

    redux: connect redux to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
+import { connect } from "react-redux";
 import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom'
 import './App.css'
+import { AppState } from './store'
 import Expenses from './Expenses'
 import Summary from './Summary'
 
-
-const App: React.FC = () => {
+const App: React.FC<AppState> = (props) => {
   return (
     <div className="App">
       <Router>
-        <Route path="/" exact component={Expenses} />
+        <Route path="/" exact>
+          <Expenses />
+        </Route>
         <Route path="/summary" component={Summary} />
       </Router>
     </div>
   )
 }
 
-export default App;
+const mapStateToProps = (state: AppState) => ({ ...state })
+
+const mapDispatchToProps = {}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
