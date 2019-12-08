ded38afeecb0474a666a3859bfd71159934a7c75
AnuchitO <anuchitp@thoughtworks.com>
2019-12-09 00:15:44 +0700

    change history to list

--- src/ExpensesHistory.tsx
+++ src/ExpensesHistory.tsx
@@ -2,82 +2,137 @@ import React, { Fragment } from 'react'
 import { Record } from './store/expenses/types'
 import uuid from 'uuid/v4'
 import {
-  Paper,
+  Avatar,
+  Button,
+  Container,
+  Divider,
   FormControlLabel,
+  Grid,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+  FormHelperText,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableRow,
-  Button,
 } from '@material-ui/core'
 
-import { makeStyles } from '@material-ui/core/styles'
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Fab from '@material-ui/core/Fab';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import MenuIcon from '@material-ui/icons/Menu';
+import AddIcon from '@material-ui/icons/Add';
+import SearchIcon from '@material-ui/icons/Search';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import FastfoodOutlinedIcon from '@material-ui/icons/FastfoodOutlined';
+
 
 type Props = {
   records: Record[]
 }
 
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-    overflowX: 'auto',
-  },
-  table: {
-    // minWidth: 650,
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    amount: {
+      color: `rgb(102, 103, 100, 1)`,
+      fontWeight: 900,
+    },
+    description: {
+      margin: 0,
+    },
+    center: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    text: {
+    },
+    paper: {
+      paddingBottom: 50,
+    },
+    list: {
+      marginBottom: theme.spacing(1),
+    },
+    subheader: {
+      backgroundColor: theme.palette.background.paper,
+    },
+    appBar: {
+      top: 'auto',
+      bottom: 0,
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    fabButton: {
+      position: 'absolute',
+      zIndex: 1,
+      top: -30,
+      left: 0,
+      right: 0,
+      margin: '0 auto',
+    },
+  }),
+);
 
 
 const ExpensesHistory: React.FC<Props> = ({ records }) => {
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
-
-    < Fragment >
-      {
-        // records.map(record => (
-        //   <div key={record.id}>
-        //     <h3>{record.amount}</h3>
-        //     <div>
-        //       {record.payer.name}
-        //       paid for
-        //       <div>
-        //         {record.owes.map(o => o.name).join(",")}
-        //       </div>
-        //     </div>
-        //   </div>
-        // ))
-
-        < Paper className={classes.root} >
-          <Table stickyHeader className={classes.table} size="small" aria-label="history">
-            <TableHead>
-              <TableRow>
-                <TableCell align="right">Payer</TableCell>
-                <TableCell align="right">Amount</TableCell>
-                <TableCell>Paid for</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {records.map(record => (
-                <TableRow key={uuid()}>
-                  <TableCell component="th" scope="row">
-                    {record.payer.name}
-                  </TableCell>
-                  <TableCell align="right">{record.amount}</TableCell>
-                  <TableCell align="right"> {record.owes.map(o => o.name).join(",")}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </Paper >
-      }
-      <Button color='primary' >view</Button>
-      {/* <FormControlLabel
-        control={}
-        label="Dense padding"
-      /> */}
-    </Fragment >
+    <Fragment>
+      <Paper square>
+        <Typography variant="h6" gutterBottom>
+          History
+        </Typography>
+        <List className={classes.list}>
+          <Fragment>
+            {/* {id === 1 && <ListSubheader className={classes.subheader}>Today</ListSubheader>}
+              {id === 3 && <ListSubheader className={classes.subheader}>Yesterday</ListSubheader>} */}
+            <ListItem>
+              <Grid container>
+                <Grid item xs={12}>
+                  {
+                    records.map((record: Record) => (
+                      <Grid container justify="center" spacing={1} key={uuid()}>
+                        <Grid item xs={1} className={classes.center}>
+                          <FastfoodOutlinedIcon />
+                        </Grid>
+                        <Grid item xs={8}>
+                          <Typography variant="button">
+                            {record.payer.name}
+                          </Typography>
+                          <FormHelperText className={classes.description}>
+                            paid for : {record.owes.map(o => o.name).join(",")}
+                          </FormHelperText>
+                          <Typography variant="caption">
+                            {record.note}
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={3} className={classes.center}>
+                          <Typography className={classes.amount} variant="h6" gutterBottom>
+                            ฿{record.amount}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    ))
+                  }
+                </Grid>
+              </Grid>
+            </ListItem>
+            <Divider variant="middle" />
+          </Fragment>
+        </List>
+      </Paper>
+    </Fragment>
   )
 }
 
