afec3f68923112c447d35ea8dc76f181d21d9dc4
AnuchitO <anuchitp@thoughtworks.com>
2019-12-08 22:53:48 +0700

    Draft NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,145 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { connect } from "react-redux";
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  withRouter
 } from 'react-router-dom'
 import './App.css'
-import Container from '@material-ui/core/Container';
+import {
+  AppBar,
+  Button,
+  Container,
+  Fab,
+  Grid,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import {
+  Search as SearchIcon,
+  Menu as MenuIcon,
+  Add as AddIcon,
+  More as MoreIcon,
+} from '@material-ui/icons'
 import { AppState } from './store'
 import Expenses from './Expenses'
 import Summary from './Summary'
 
-const Nav = () => (
-  <div>
-    <ul>
-      <li><Link to="/">Expenses</Link></li>
-      <li><Link to="/summary">Summary</Link></li>
-    </ul>
-  </div>
+const SimpleMenu: React.FC = () => {
+
+  return (
+    <div>
+      <Button aria-controls="simple-menu" aria-haspopup="true">
+        Open Menu
+      </Button>
+
+    </div>
+  );
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    appBar: {
+    }
+  })
 );
+
+type NavMenuProps = {
+  naviagateTo: (path: string) => any
+}
+
+const NavMenu: React.FC<NavMenuProps> = (props) => {
+  const classes = useStyles()
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = (path: string) => {
+    setAnchorEl(null);
+    props.naviagateTo(path)
+  };
+
+  return (
+    <Fragment>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <IconButton onClick={handleMenu} edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={() => handleClose("/")}>Expenses</MenuItem>
+              <MenuItem onClick={() => handleClose("/summary")}>Summary</MenuItem>
+            </Menu>
+          </IconButton>
+          <Typography variant="h6" >
+            Jugbaht
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Fragment >
+  )
+}
+
+type NavComponentProps = {
+  history: any
+}
+class NavComponent extends React.Component<NavComponentProps> {
+  render() {
+    return (
+      <Fragment>
+        <NavMenu naviagateTo={(path: string) => { this.props.history.push(path) }} />
+      </Fragment>
+    )
+  }
+}
+
+const Nav = withRouter(connect(
+  (state: AppState) => ({}),
+  {})(NavComponent));
 
 const App: React.FC<AppState> = (props) => {
   return (
     <div className="App">
-      <Container maxWidth="md">
-        <Router>
-          <Nav />
-          <Route path="/" exact component={Expenses} />
-          <Route path="/summary" component={Summary} />
-        </Router>
-      </Container>
+      <Router>
+        <Container maxWidth="md">
+          <Grid container>
+            <Grid item xs={12}>
+              {/* TODO: Add Nav bar redirect. */}
+              {/* <Nav />  */}
+              <ul>
+                <li><Link to="/">Expenses</Link></li>
+                <li><Link to="/summary">Summary</Link></li>
+              </ul>
+            </Grid>
+            <Grid item xs={12}>
+              <Route path="/" exact component={Expenses} />
+              <Route path="/summary" component={Summary} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Router>
+
     </div>
   )
 }
