5fa227678dc6f43ccf3305d66b308daf9b94ea39
AnuchitO <anuchitp@thoughtworks.com>
2019-12-11 14:50:41 +0700

    Revert "add type number"
    This reverts commit e24dcc4ce7927787c4228ed46f86ca79504e9c21.

--- src/NumberFormatInput.tsx
+++ src/NumberFormatInput.tsx
@@ -69,7 +69,6 @@ export const FormattedInputs: React.FC<Props> = (props) => {
         <TextField
           label="amount"
           id="id-input-amount"
-          type='number'
           value={values.numberformat}
           onChange={handleChange("numberformat")}
           onKeyUp={props.onKeyUp}
