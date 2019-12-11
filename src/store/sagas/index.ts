5948449b85eb83a2e08c7d2e98e17f9d0341b56c
AnuchitO <anuchitp@thoughtworks.com>
2019-12-12 00:20:37 +0700

    remove console.log

--- src/store/sagas/index.ts
+++ src/store/sagas/index.ts
@@ -6,7 +6,6 @@ import { LOAD_EXPENSE_RECORDS, INIT_EXPENSE_RECORDS } from '../expenses/types';
 const key = "records"
 
 export function* initExpenseRecords() {
-  console.log("Helloooo")
   const recoredRaw: string = localStorage.getItem(key) || '[]';
   const records: Record[] = JSON.parse(recoredRaw)
   yield put({ type: INIT_EXPENSE_RECORDS, payload: records });
