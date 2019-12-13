4ad6512c1cf8f62595253572cab5706e51af0cc6
AnuchitO <anuchitp@thoughtworks.com>
2019-12-13 13:57:40 +0700

    loadddd

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -41,7 +41,7 @@ export function register(config?: Config) {
 
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
+      console.log("loaddddddd")
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
